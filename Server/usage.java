dad3dbd1d0592024de7965d736d8fee92868b2ff
Ivan Dortulov <ivandortulov@yahoo.com>
2015-04-11 16:14:10 +0300

    Updated usage documentation.

--- Server/usage.java
+++ Server/usage.java
@@ -15,31 +15,76 @@
 */
 		String URL = "http://127.0.0.1:27015/register?fb_id='123456'&real_name='Ivan Dortulov'";
 		HttpClient httpclient = new DefaultHttpClient();
-    	HttpResponse response = httpclient.execute(new HttpGet(URL));
-    	StatusLine statusLine = response.getStatusLine();
-    	if(statusLine.getStatusCode() == HttpStatus.SC_OK){
-    		ByteArrayOutputStream out = new ByteArrayOutputStream();
-    		response.getEntity().writeTo(out);
-    		String responseString = out.toString();
-    		out.close();
+		HttpResponse response = httpclient.execute(new HttpGet(URL));
+		StatusLine statusLine = response.getStatusLine();
+		if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			response.getEntity().writeTo(out);
+			String responseString = out.toString();
+			out.close();
     		
-    		switch(Integer.parseInt(responseString[0])) {
-    			case 1: {
-    				// User was registered without errors.
-    				break;
-    			} case 2: {
-    				// The user already registered.
-    				break;
-    			} case 3: {
-    				// Error has occured
-    				Log.e("Client", "Error occured: " + responseString);
-    				break;
-    			}
-    		}
-    	} else{
-    	    //Closes the connection.
-    		response.getEntity().getContent().close();
+			switch(Integer.parseInt(responseString[0])) {
+				case 1: {
+					// User was registered without errors.
+					break;
+				} case 2: {
+					// The user already registered.
+					break;
+				} case 3: {
+					// Error has occured
+					Log.e("Client", "Error occured: " + responseString);
+					break;
+				}
+			}
+		} else{
+			//Closes the connection.
+			response.getEntity().getContent().close();
 			throw new IOException(statusLine.getReasonPhrase());
-    	}
+		}
     	
+/* 2. Get a random picture
+	
+	You need to make a request to the URL: http://server_ip:27015/get_pic?<params>
+	where params are parameters for the picture selection
+	
+	- fb_id - The user's facebook id
+	- category - The pictures category
+	
+	Exaple: http://127.0.0.1:27015/get_pic?fb_id='123456'&category='space'
+	
+	The server will return an image.
+	
+	Example code:
+*/
 
+	String URL = "http://127.0.0.1:27015/get_pic?fb_id='123456'&category='space'"HttpClient httpclient = new DefaultHttpClient();
+	HttpResponse response = httpclient.execute(new HttpGet(URL));
+	StatusLine statusLine = response.getStatusLine();
+	if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		response.getEntity().writeTo(out);
+		String responseString = out.toString();
+		out.close();
+	
+		String[] lines = responseString.readlines();
+		String imageId = Integer.parseInt(lines[0]);
+		String imageURL = lines[1];
+	
+		Picasso.with(context).load(imageURL).into(imageView);
+	} else{
+		//Closes the connection.
+		response.getEntity().getContent().close();
+		throw new IOException(statusLine.getReasonPhrase());
+	}
+	
+/* 3. Give a space
+
+	You need to make a request to the UR: http://server_ip:27015/space?<params>
+	where params are parameters for the picture selection
+	
+	- fb_id - User's facebook id
+	- pic_id - Pictures id
+	- space - Space or not (1 or 0)
+	
+	Example: http://127.0.0.1:27015/space?fb_id='123456'&pic_id='15'&space='1'
+*/
